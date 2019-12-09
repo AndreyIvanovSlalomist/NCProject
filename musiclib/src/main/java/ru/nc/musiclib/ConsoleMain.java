e86230bd42cb5685c05872639426d1cd1b426cb6
Андрей <slalomist22@yandex.ru>
2019-12-09 12:21:23 +0400

    2 jar

---
+++ musiclib/src/main/java/ru/nc/musiclib/ConsoleMain.java
@@ -0,0 +1,22 @@
+package ru.nc.musiclib;
+
+import ru.nc.musiclib.controller.Controller;
+import ru.nc.musiclib.controller.impl.MusicLibController;
+import ru.nc.musiclib.model.Model;
+import ru.nc.musiclib.model.impl.MusicModel;
+import ru.nc.musiclib.view.View;
+import ru.nc.musiclib.view.impl.ConsoleView;
+
+public class ConsoleMain {
+    public static void main(String[] args) {
+
+        View view = new ConsoleView();
+        Controller controller = new MusicLibController();
+        Model model = new MusicModel();
+        view.setController(controller);
+        view.setModel(model);
+        controller.setModel(model);
+        controller.setView(view);
+        view.runView();
+    }
+}
--- musiclib/src/main/java/ru/nc/musiclib/ServerMain.java
+++ musiclib/src/main/java/ru/nc/musiclib/ServerMain.java
@@ -6,16 +6,8 @@ import ru.nc.musiclib.model.Model;
 import ru.nc.musiclib.model.impl.MusicModel;
 import ru.nc.musiclib.net.server.MusicLibServer;
 
-public class Main {
+public class ServerMain {
     public static void main(String[] args) {
-      /*  View view = new ConsoleView();
-        Controller controller = new MusicLibController();
-        Model model = new MusicModel();
-        view.setController(controller);
-        view.setModel(model);
-        controller.setModel(model);
-        controller.setView(view);
-        view.runView();*/
         int port;
         if (args.length == 1) {
             port = Integer.parseInt(args[0]);
