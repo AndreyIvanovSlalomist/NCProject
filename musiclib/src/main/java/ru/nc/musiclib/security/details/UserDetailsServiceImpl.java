e26d19c51542bf5d0f358062fd9a3eac87aca59c
Андрей <slalomist22@yandex.ru>
2020-04-10 16:25:02 +0400

    Merge branch 'master' of https://github.com/AndreyIvanovSlalomist/NCProject
    # Conflicts:
    #	musiclib/src/main/java/ru/nc/musiclib/controller/UserController.java
    #	musiclib/src/main/java/ru/nc/musiclib/services/impl/UsersModelWithDao.java

---
+++ musiclib/src/main/java/ru/nc/musiclib/security/details/UserDetailsServiceImpl.java
@@ -0,0 +1,14 @@
+package ru.nc.musiclib.security.details;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import ru.nc.musiclib.services.UserModel;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+       return null;
+    }
+}
