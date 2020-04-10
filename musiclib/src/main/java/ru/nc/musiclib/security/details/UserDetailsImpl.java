d7804e5f844b672dd135ac7bc61c59dab900eb16
Андрей <slalomist22@yandex.ru>
2020-04-10 17:01:37 +0400

    Merge branch 'master' of https://github.com/AndreyIvanovSlalomist/NCProject
    # Conflicts:
    #	musiclib/src/main/java/ru/nc/musiclib/security/details/UserDetailsServiceImpl.java

--- musiclib/src/main/java/ru/nc/musiclib/security/details/UserDetailsImpl.java
+++ musiclib/src/main/java/ru/nc/musiclib/security/details/UserDetailsImpl.java
@@ -1,48 +1,58 @@
 package ru.nc.musiclib.security.details;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import ru.nc.musiclib.model.User;
 
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
 
 public class UserDetailsImpl implements UserDetails {
-    @Autowired
+
     private User user;
+
+    public UserDetailsImpl(User user) {
+        this.user = user;
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority(user.getRole().getRoleName());
+        Set<SimpleGrantedAuthority> authorities = new HashSet<>();
+        authorities.add(authority);
+        return authorities;
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return user.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return user.getUserName();
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
