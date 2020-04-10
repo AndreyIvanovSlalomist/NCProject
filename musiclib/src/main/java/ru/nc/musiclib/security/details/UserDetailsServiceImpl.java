40fe26366d0196df3fe66d0348e005e4394c0440
yanakhugaeva <52502401+yanakhugaeva@users.noreply.github.com>
2020-04-10 16:48:15 +0400

    userdetailsimpl и userdetailsserviceimpl методы

--- musiclib/src/main/java/ru/nc/musiclib/security/details/UserDetailsServiceImpl.java
+++ musiclib/src/main/java/ru/nc/musiclib/security/details/UserDetailsServiceImpl.java
@@ -1,14 +1,25 @@
 package ru.nc.musiclib.security.details;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ru.nc.musiclib.model.User;
 import ru.nc.musiclib.services.UserModel;
 
-public class UserDetailsServiceImpl implements UserDetailsService {
 
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserModel userModel;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-       return null;
+        User user = userModel.findUser(username);
+        if(user == null) {
+            throw new UsernameNotFoundException("Пользователь не найден!");
+        }
+        UserDetailsImpl userDetails = new UserDetailsImpl(user);
+       return new org.springframework.security.core.userdetails.User(userDetails.getUsername(), userDetails.getPassword(), userDetails.getAuthorities());
     }
 }
