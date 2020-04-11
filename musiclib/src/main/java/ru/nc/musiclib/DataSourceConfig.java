6e400a38ea2a53a46cb106657bbd6ed3bb6bf01e
Андрей <slalomist22@yandex.ru>
2020-04-11 16:49:47 +0400

    DataSource с вазможностью задания пользовательский настроек в файле %CATALINA_HOME%/conf/musiclib_custom.properties

---
+++ musiclib/src/main/java/ru/nc/musiclib/DataSourceConfig.java
@@ -0,0 +1,67 @@
+package ru.nc.musiclib;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.io.*;
+import java.util.Properties;
+
+@Configuration
+public class DataSourceConfig {
+    private static final String PROPERTIES_PATH = "musiclib.properties";
+    private static final String CUSTOM_PROPERTIES_PATH = "musiclib_custom.properties";
+
+    private static String getPropertiesInFile(String nameProperty, String propertiesPath) throws IOException {
+        Properties properties = new Properties();
+        InputStream fileInputStream;
+        try {
+            fileInputStream = new FileInputStream(propertiesPath);
+            properties.load(fileInputStream);
+            return properties.getProperty(nameProperty);
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден " + propertiesPath);
+        }
+        return null;
+    }
+
+    private static String getProperty(String nameProperty) {
+        String filePath = System.getenv().get("CATALINA_HOME") + File.separator + "conf" + File.separator + CUSTOM_PROPERTIES_PATH;
+        try {
+            String valueProperty = getPropertiesInFile(nameProperty, filePath);
+            if (valueProperty == null) {
+                filePath = DataSourceConfig.class.getClassLoader().getResource(PROPERTIES_PATH).getPath();
+                valueProperty = getPropertiesInFile(nameProperty, filePath);
+            }
+            System.out.println(" Имя параметра " + nameProperty + " Значение параметра " + valueProperty + " Из файла " + filePath);
+            return valueProperty;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    @Bean
+    public DataSource getDataSource() {
+        System.out.println("Создаем свой DataSours");
+        String ip = getProperty("db.ip");
+        String port = getProperty("db.port");
+        String baseName = getProperty("db.baseName");
+        String username = getProperty("db.username");
+        String password = getProperty("db.Password");
+
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.postgresql.Driver");
+        dataSourceBuilder.url("jdbc:postgresql://" + ip + ":" + port + "/" + baseName);
+        dataSourceBuilder.username(username);
+        dataSourceBuilder.password(password);
+
+        String adminUsername = getProperty("admin.username");
+        String adminPassword = getProperty("admin.Password");
+
+
+        return dataSourceBuilder.build();
+    }
+
+}
